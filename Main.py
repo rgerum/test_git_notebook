47df869e22d5e1b5a8a9e2b7474184be34be97cd
Richard Gerum <14153051+rgerum@users.noreply.github.com>
2023-05-18 11:54:19 -0400

    added main.py

---
+++ Main.py
@@ -0,0 +1,34 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:light
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.14.5
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+# ---
+
+# # Main
+
+print("hello wooooooooooooorld")
+
+# ## subsection
+
+# +
+print("new")
+
+print("this is great")
+
+print("new2")
+# -
+
+a = 10*100
+
+a
+
+
